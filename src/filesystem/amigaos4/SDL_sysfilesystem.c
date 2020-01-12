f141b5fbae0ad72a2548902be7dabccec1ecf3cf
capehill <juha.niemimaki@gmail.com>
2020-01-12 12:52:58 +0200

    Attempt to create directories when SDL_GetPrefPath() is called

--- src/filesystem/amigaos4/SDL_sysfilesystem.c
+++ src/filesystem/amigaos4/SDL_sysfilesystem.c
@@ -1,6 +1,6 @@
 /*
   Simple DirectMedia Layer
-  Copyright (C) 1997-2017 Sam Lantinga <slouken@libsdl.org>
+  Copyright (C) 1997-2020 Sam Lantinga <slouken@libsdl.org>
 
   This software is provided 'as-is', without any express or implied
   warranty.  In no event will the authors be held liable for any damages
@@ -27,16 +27,38 @@
 
 #include "SDL_error.h"
 #include "SDL_filesystem.h"
+#include "../../video/amigaos4/SDL_os4library.h"
 
-#define OS4_BASE_PATH "PROGDIR:"
+#define DEBUG
+#include "../../main/amigaos4/SDL_os4debug.h"
+
+#include <proto/dos.h>
+
+static struct DOSIFace* iDos;
+static struct DOSBase* dosBase;
+
+static BOOL
+OS4_OpenDosLibrary()
+{
+    dosBase = (struct DOSBase *)OS4_OpenLibrary("dos.library", 50);
+    iDos = (struct DOSIFace *)OS4_GetInterface((struct Library *)dosBase);
+    return iDos != NULL;
+}
+
+static void
+OS4_CloseDosLibrary()
+{
+    OS4_DropInterface((struct Interface **)&iDos);
+    OS4_CloseLibrary((struct Library **)&dosBase);
+}
 
 char *
 SDL_GetBasePath(void)
 {
-    size_t len = 0;
     char *buffer = NULL;
+    const char* const basePath = "PROGDIR:";
 
-    len = SDL_strlen(OS4_BASE_PATH) + 1;
+    size_t len = SDL_strlen(basePath) + 1;
 
     buffer = (char *) SDL_malloc(len);
     if (!buffer) {
@@ -45,9 +67,54 @@ SDL_GetBasePath(void)
     }
 
     SDL_memset(buffer, 0, len);
-    SDL_snprintf(buffer, len, "%s", OS4_BASE_PATH);
+    SDL_snprintf(buffer, len, "%s", basePath);
 
     return buffer;
+}
+
+static BOOL
+OS4_CreateDirTree(const char* path)
+{
+    BOOL success = FALSE;
+
+    char* temp = SDL_strdup(path);
+
+    if (!temp) {
+        dprintf("Failed to create temporary path\n");
+        return FALSE;
+    }
+
+    const size_t len = SDL_strlen(temp);
+
+    if (len < 1) {
+        dprintf("Empty string\n");
+        return FALSE;
+    }
+
+    if (temp[len - 1] == '/') {
+        temp[len - 1] = '\0';
+    }
+
+    if (OS4_OpenDosLibrary()) {
+        BPTR lock = iDos->CreateDirTree(temp);
+        if (lock) {
+            success = TRUE;
+            iDos->UnLock(lock);
+        } else {
+            const int32 err = iDos->IoErr();
+            dprintf("Failed to create dir tree '%s' (err %d)\n", temp, err);
+            if (err == ERROR_OBJECT_EXISTS) {
+                dprintf("Object already exists -> success\n");
+                success = TRUE;
+            }
+        }
+
+        OS4_CloseDosLibrary();
+    }
+
+    SDL_free(temp);
+
+    return success;
 }
 
 char *
@@ -69,8 +136,8 @@ SDL_GetPrefPath(const char *org, const char *app)
         SDL_OutOfMemory();
         return NULL;
     }
-    SDL_memset(buffer, 0, len);
 
+    SDL_memset(buffer, 0, len);
     SDL_snprintf(buffer, len, "ENV:");
 
     if (org) {
@@ -81,7 +148,12 @@ SDL_GetPrefPath(const char *org, const char *app)
         SDL_snprintf(buffer + SDL_strlen(buffer), len - SDL_strlen(buffer), "%s/", app);
     }
 
-    return buffer;
+    if (OS4_CreateDirTree(buffer)) {
+        return buffer;
+    }
+
+    SDL_free(buffer);
+    return NULL;
 }
 
 #endif /* SDL_FILESYSTEM_AMIGAOS4 */
