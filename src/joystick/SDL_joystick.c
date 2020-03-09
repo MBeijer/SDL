230d7f8bc314e2f0057455a4cfdaaf82a9c824ac
BeWorld <36823759+BeWorld2018@users.noreply.github.com>
2020-03-09 12:57:42 +0100

    add SDL_MORPHOS_JoystickDriver

--- src/joystick/SDL_joystick.c
+++ src/joystick/SDL_joystick.c
@@ -91,6 +91,9 @@ static SDL_JoystickDriver *SDL_joystick_drivers[] = {
 #if defined(SDL_JOYSTICK_AMIGAINPUT)
     &SDL_AMIGAINPUT_JoystickDriver,
 #endif
+#ifdef SDL_JOYSTICK_MORPHOS
+    &SDL_MORPHOS_JoystickDriver,
+#endif
 #if defined(SDL_JOYSTICK_DUMMY) || defined(SDL_JOYSTICK_DISABLED)
     &SDL_DUMMY_JoystickDriver
 #endif
