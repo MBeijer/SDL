c5e78b5facdb203824f0e70806028079b09851fb
BeWorld <36823759+BeWorld2018@users.noreply.github.com>
2020-03-11 19:45:13 +0100

    Cleanup
    - change sdk directory
    - add Szilárd Biró in version
    - clean makefile

--- src/core/morphos/SDL_amigaversion.h
+++ src/core/morphos/SDL_amigaversion.h
@@ -1,6 +1,6 @@
 #define	VERSION	53
 #define	REVISION	0
-#define	DATE	"02.03.2020"
+#define	DATE	"11.03.2020"
 #define	VERS	"sdl2.library 53.0"
-#define	VSTRING	"sdl2.library 53.0 (04.03.2020) © Ilkka Lehtoranta, Bruno Peloille\r\n"
-#define	VERSTAG	"\0$VER: sdl2.library 53.0 (04.03.2020) © Ilkka Lehtoranta, Bruno Peloille"
+#define	VSTRING	"sdl2.library 53.0 (11.03.2020) © Ilkka Lehtoranta, Bruno Peloille, Szilárd Biró\r\n"
+#define	VERSTAG	"\0$VER: sdl2.library 53.0 (11.03.2020) © Ilkka Lehtoranta, Bruno Peloille, Szilárd Biró"
