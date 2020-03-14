6cb959d0cda190d977bf46867b008e8b80645d72
BeWorld <36823759+BeWorld2018@users.noreply.github.com>
2020-03-14 17:39:51 +0100

    Disable Resize Gadget on Window
    TODO later

--- src/video/amiga/SDL_amigawindow.h
+++ src/video/amiga/SDL_amigawindow.h
@@ -90,9 +90,9 @@ extern SDL_bool AMIGA_GetWindowWMInfo(_THIS, SDL_Window * window, struct SDL_Sys
 extern void AMIGA_ShowWindow_Internal(SDL_Window * window);
 extern void AMIGA_HideWindow_Internal(SDL_Window * window);
 
-extern void AMIGA_SetWindowResizable (_THIS, SDL_Window * window, SDL_bool resizable);
-extern int AMIGA_SetWindowOpacity(_THIS, SDL_Window * window, float opacity);
-extern int AMIGA_GetWindowBordersSize(_THIS, SDL_Window * window, int * top, int * left, int * bottom, int * right);
+//extern void AMIGA_SetWindowResizable (_THIS, SDL_Window * window, SDL_bool resizable);
+//extern int AMIGA_SetWindowOpacity(_THIS, SDL_Window * window, float opacity);
+//extern int AMIGA_GetWindowBordersSize(_THIS, SDL_Window * window, int * top, int * left, int * bottom, int * right);
 
 
 #endif /* _SDL_amigawindow_h */
