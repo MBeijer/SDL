bc391c06d0fb9a71beb2ae0d59a71aab878ee0e8
BeWorld <36823759+BeWorld2018@users.noreply.github.com>
2020-04-01 14:49:17 +0200

    Update version for first public beta release

--- src/core/morphos/SDL_amigaversion.h
+++ src/core/morphos/SDL_amigaversion.h
@@ -1,6 +1,6 @@
 #define	VERSION	53
 #define	REVISION	0
-#define	DATE	"11.03.2020"
+#define	DATE	"31.03.2020"
 #define	VERS	"sdl2.library 53.0"
-#define	VSTRING	"sdl2.library 53.0 (11.03.2020) © Ilkka Lehtoranta, Bruno Peloille, Szilárd Biró\r\n"
-#define	VERSTAG	"\0$VER: sdl2.library 53.0 (11.03.2020) © Ilkka Lehtoranta, Bruno Peloille, Szilárd Biró"
+#define	VSTRING	"sdl2.library 53.0 beta (31.03.2020) Â© Ilkka Lehtoranta, Bruno Peloille, SzilÃ¡rd BirÃ³\r\n"
+#define	VERSTAG	"\0$VER: sdl2.library 53.0 beta (31.03.2020) Â© Ilkka Lehtoranta, Bruno Peloille, SzilÃ¡rd BirÃ³"
